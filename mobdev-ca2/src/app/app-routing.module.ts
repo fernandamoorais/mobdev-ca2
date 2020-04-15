b8e555ee493eb759f49805c39d120bcd18230e7f
fernandamoorais <fernandamoorais@hotmail.com>
2020-04-15 14:46:20 +0000

    correction on tabes-routing.module.ts

--- mobdev-ca2/src/app/app-routing.module.ts
+++ mobdev-ca2/src/app/app-routing.module.ts
@@ -1,69 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { TabsPage } from './tabs.page';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
 const routes: Routes = [
-    {
-        path: 'tabs',
-        component: TabsPage,
-        children: [
-            {
-                path: 'episodes',
-                children: [
-                    {
-                        path: '',
-                        loadChildren: () => import('./pages/episodes/episodes.module').then(m => m.EpisodesPageModule)
-                    },
-                    {
-                        path: ':id',
-                        loadChildren: () => import('./pages/episode-details/episode-details.module').then(m => m.EpisodeDetailsPageModule)
-                    }
-                ]
-            },
-            {
-                path: 'characters',
-                children: [
-                    {
-                        path: '',
-                        loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersPageModule)
-                    },
-                    {
-                        path: ':id',
-                        loadChildren: () => import('../character-details/character-details.module').then(m => m.CharacterDetailsPageModule)
-                    }
-                ]
-            },
-            {
-                path: 'quotes',
-                children: [
-                    {
-                        path: '',
-                        loadChildren: () => import('./pages/quotes/quotes.module').then(m => m.QuotesPageModule)
-                    },
-                    {
-                        path: ':id',
-                        loadChildren: () => import('./pages/quote-details/quote-details.module').then(m => m.QuoteDetailsPageModule)
-                    }
-                ]
-            },
-            {
-                path: 'deaths',
-                children: [
-                    {
-                        path: '',
-                        loadChildren: () => import('./pages/deaths/deaths.module').then(m => m.DeathsPageModule)
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        path: '',
-        redirectTo: '/tabs/episodes',
-        pathMatch: 'full'
-    }
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
+
+
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
 })
-export class TabsPageRoutingModule { }
+export class AppRoutingModule { }
