406506aed4c6e370ad12d990460d693e4d42604a
fernandamoorais <fernandamoorais@hotmail.com>
2020-04-26 21:43:13 +0000

    FavoriteEp button

--- mobdev-ca2/src/app/services/like.service.ts
+++ mobdev-ca2/src/app/services/like.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
-const STORAGE_KEY = 'likeEpisodes';
+const STORAGE_KEY = 'likeCharacter';
+const STORAGE_KEY2 = 'likeEpisodes;'
 
 @Injectable({
     providedIn: 'root'
@@ -10,31 +11,63 @@ export class LikeService {
 
     constructor(private storage: Storage) { }
 
-    getAllLikeEpisodes() {
-        return this.storage.get(STORAGE_KEY);
+    getAllFavouriteEps() {
+        return this.storage.get(STORAGE_KEY2);
     }
 
-    Like(episodeId) {
-        return this.getAllLikeEpisodes().then(result => {
+    isFav(episodeId) {
+        return this.getAllFavouriteEps().then(result => {
             return result && result.indexOf(episodeId) !== -1;
         });
     }
 
-    likeEpisode(episodeId) {
-        return this.getAllLikeEpisodes().then(result => {
+    favouriteEp(episodeId) {
+        return this.getAllFavouriteEps().then(result => {
             if (result) {
                 result.push(episodeId);
-                return this.storage.set(STORAGE_KEY, result);
+                return this.storage.set(STORAGE_KEY2, result);
             } else {
-                return this.storage.set(STORAGE_KEY, [episodeId]);
+                return this.storage.set(STORAGE_KEY2, [episodeId]);
             }
         });
     }
 
-    dislikeEpisode(episodeId) {
-        return this.getAllLikeEpisodes().then(result => {
+    unfavouriteEp(episodeId) {
+        return this.getAllFavouriteEps().then(result => {
             if (result) {
                 var index = result.indexOf(episodeId);
+                result.splice(index, 1);
+                return this.storage.set(STORAGE_KEY2, result);
+            }
+        });
+    }
+    //Same Logic but Know for Like/Dislike for Characters 
+
+    getAllLikeCharacters() {
+        return this.storage.get(STORAGE_KEY);
+    }
+
+    Like(charId) {
+        return this.getAllLikeCharacters().then(result => {
+            return result && result.indexOf(charId) !== -1;
+        });
+    }
+
+    likeCharacters(charId) {
+        return this.getAllLikeCharacters().then(result => {
+            if (result) {
+                result.push(charId);
+                return this.storage.set(STORAGE_KEY, result);
+            } else {
+                return this.storage.set(STORAGE_KEY, [charId]);
+            }
+        });
+    }
+
+    dislikeCharacters(charId) {
+        return this.getAllLikeCharacters().then(result => {
+            if (result) {
+                var index = result.indexOf(charId);
                 result.splice(index, 1);
                 return this.storage.set(STORAGE_KEY, result);
             }
